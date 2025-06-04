ec0de981723a1c87f5ba18c439a614c994212722
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2025-06-04 08:59:13 +0300

    Added Last Answer panel in LearnPage

--- SteelFlashcards/LearnViewModel.cs
+++ SteelFlashcards/LearnViewModel.cs
@@ -25,12 +25,16 @@ namespace LanguageLearn2
 
         public ObservableCollection<Answer> Answers { get { return answers; } }
 
+        [ObservableProperty]
+        private Answer? lastAnswer;
+
         public LearnViewModel(IDataService dataService, INavigationService navigationService)
         {
             _dataService = dataService;
             _navigationService = navigationService;
             m_words = _dataService.GetWords();
             m_randomGenerator = new Random();
+            LastAnswer = null;
             SetNextWord();
         }
 
@@ -44,6 +48,7 @@ namespace LanguageLearn2
             Answer answer = CreateAnswer(guess);
             answers.Add(answer);
             _dataService.AddAnswer(answer);
+            LastAnswer = answer;
             SetNextWord();
         }
 
@@ -53,6 +58,7 @@ namespace LanguageLearn2
             _dataService.ClearAnswers();
             answers.Clear();
             Answer.Reset();
+            LastAnswer = null;
         }
 
         private void SetNextWord()
@@ -113,14 +119,13 @@ namespace LanguageLearn2
             {
                 for (int j = 1; j <= word2.Length; j++)
                 {
-                    // Common mistypes like 1 -> a_nosine, or a -> a_nosine could be detected in here as well
+                    // TODO: Common mistypes like 1 -> a_nosine, or a -> a_nosine could be detected in here as well
                     int substitutionCost = word1[i - 1] == word2[j - 1] ? 0 : 1;
                     distance[i, j] = Math.Min(distance[i - 1, j] + 1, Math.Min(distance[i, j - 1] + 1, distance[i - 1, j - 1] + substitutionCost));
                 }
             }
             return distance[word1.Length, word2.Length];
         }
-
     }
 
     public class BoolToIconConverter : IValueConverter
